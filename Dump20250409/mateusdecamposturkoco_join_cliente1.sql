82e713dba618a0e606e8dd4fe522071cd175398f
dev.mateus <mateusdecamposturkoco@gmail.com>
2025-04-09 21:35:11 -0300

    att

---
+++ Dump20250409/mateusdecamposturkoco_join_cliente1.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mateusdecamposturkoco_join
+-- ------------------------------------------------------
+-- Server version	8.0.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cliente1`
+--
+
+DROP TABLE IF EXISTS `cliente1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente1` (
+  `Cod_Cliente` int NOT NULL,
+  `Nome_Cliente` varchar(60) NOT NULL,
+  `Data_Nascimento` date DEFAULT NULL,
+  `Telefone` char(9) DEFAULT NULL,
+  `Cod_Profissao` int DEFAULT NULL,
+  PRIMARY KEY (`Cod_Cliente`),
+  KEY `Cod_Profissao` (`Cod_Profissao`),
+  CONSTRAINT `cliente1_ibfk_1` FOREIGN KEY (`Cod_Profissao`) REFERENCES `profissao` (`cod_Profissao`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente1`
+--
+
+LOCK TABLES `cliente1` WRITE;
+/*!40000 ALTER TABLE `cliente1` DISABLE KEYS */;
+INSERT INTO `cliente1` VALUES (121,'João','1980-09-20','3456-7890',25),(122,'Maria','1972-01-22','3456-7891',26),(123,'Mateus','1983-04-29','3456-7892',27),(124,'Gustavo','1990-01-12','3456-7894',28);
+/*!40000 ALTER TABLE `cliente1` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-09 21:25:33
